872212495c1366bd69851b1c37a85e8cb3a81030
Felipe Oliveira <felipe2.oliveira@gmail.com>
2023-01-07 16:11:37 -0300

    initial commit

---
+++ components/List.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import { Task } from "../types/Task";
+import { Item } from "./Item";
+
+type ListProps = {
+    tasks: Task[]
+}
+
+export const List: NextPage<ListProps> = ({tasks}) => {
+    return (
+        <div className={"container-list" + (tasks && tasks.length > 0 ? ' not-empty' : '')}>
+            {tasks && tasks.length > 0 
+                ? tasks.map(t => <Item key={t._id} task={t} />)
+                : (
+                    <>
+                        <img src="/empty.svg" alt="Nenhuma atividade encontrada" />
+                        <p>Você ainda não possui tarefas cadastradas!</p>
+                    </>   
+                )}
+        </div>
+    );
+}
